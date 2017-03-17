628cb620c7cdfdb4d8fd705be7b565e6e844c245
Mohamed Elmi <maxto024@gmail.com>
2017-03-17 22:03:04 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,284 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+<head>
+<meta charset="UTF-8" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="initial-scale=1, maximum-scale=1" />
+<meta name="viewport" content="width=device-width" />
+<title>Online Shoping</title>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" href="css/reset.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="css/layout.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="css/jqueryslidemenu.css" type="text/css" media="screen" />
+
+<!-- supersized -->
+<link rel="stylesheet" href="css/supersized.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="css/supersized.shutter.css" type="text/css" media="screen" />
+<!-- supersized -->
+
+<link rel="stylesheet" href="css/carouFredSel.css" type="text/css" media="screen" />
+<link rel="stylesheet" type="text/css" href="css/quicksand.css" />
+<link rel="stylesheet" type="text/css" href="js/fancybox/jquery.fancybox-1.3.4.css" media="screen" />
+<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/jqueryslidemenu.js"></script>
+<script type="text/javascript" src="js/jquery.easing.min.js"></script>
+
+<!-- supersized -->
+<script type="text/javascript" src="js/supersized.3.2.7.js"></script>
+<script type="text/javascript" src="js/supersized.shutter.js"></script>
+<!-- supersized -->
+
+<!-- fancybox -->
+<script type="text/javascript" src="js/fancybox/jquery.fancybox-1.3.4.js"></script>
+<script type="text/javascript" src="js/fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
+<!-- fancybox -->
+
+<!-- quicksand -->
+<script type="text/javascript" src="js/portfolio_sortable.js"></script>
+<script type="text/javascript" src="js/quicksand.js"></script>
+<!-- quicksand -->
+
+<script type="text/javascript" src="js/jquery.carouFredSel-6.0.6.js"></script>
+
+<script type="text/javascript" src="js/contact.js"></script>
+<script type="text/javascript" src="js/custom.js"></script>
+
+            
+<!--[if lt IE 9]>
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<!--[if lt IE 9]>
+	<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+<![endif]-->
+<link href="http://fonts.googleapis.com/css?family=Oswald:400,300,600,700,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="page_wrapper">
+<div id="dvLoading"></div>
+<div id="bgOverlay"><a  href="#" class="page_close"><img src="images/close_button.png" alt="img" /></a></div>
+
+
+<!--Supersized Controls Start-->
+<div id="controls">
+    <ul>
+        <li><a id="prevslide" class="load-item"><img src="images/back.png" alt="img" /></a></li>
+        <li><a id="play-button"><img id="pauseplay" src="images/pause.png" alt="img"/></a></li>
+        <li><a id="nextslide" class="load-item"><img src="images/forward.png" alt="img" /></a></li>
+    </ul>
+</div>
+<div id="slidecaption_wrapper">
+    <div id="slidecaption"></div>
+</div>
+<!--Supersized Controls End-->
+
+
+<!--leftSide start --> 
+<div id="leftSide">
+    <div id="logo">
+        <h1>
+            <a href="index.php">
+                <img src="images/logo.png" alt="logo" />
+            </a>
+        </h1>
+    </div>
+	<ul>
+			<li style="border-bottom: 1px solid rgba(127, 127, 127, 0.3); position: relative;"><a href="store/generatetrans.php" style="color: #211C1B; display: block; font-family: 'Oswald',Arial; font-weight: 600; padding: 10px 25px; text-decoration: none; text-transform: uppercase; font-size:15px;">products</a></li>
+		</ul>
+    <nav id="sidebarmenu">
+        <ul id="sidebarmenu1" class="menu">
+            <li><a href="#page_about">about</a></li>
+            <li><a href="#page_gallery">Gallery</a></li>
+            <li><a href="#page_contact">FEEDBACK</a></li>
+			<li><a href="#adminlogin">Login</a></li>
+        </ul>
+		
+    </nav>
+    
+    <div class="social_media_wrapper"> 
+      <div class="social_media"> 
+            <ul>
+                <li></li>
+                
+            </ul>
+        </div>
+       <li><img src="images/promo.jpg" alt="img" width="215" height="120"></li>
+    </div>
+</div>
+<!--leftside end --> 
+
+
+<!--Content Start-->
+<div class="contWrapper">
+<article id="content">
+
+
+
+<ul id="ulcontent">
+
+<!--About Us Start-->
+<li id="page_about">
+    <div class="title-wrapper">
+        <h2>History</h2>
+    </div>
+    <h4>History</h4>
+    <p style="text-align:justify;">
+	  Developed by Mohamed Elmi Hassan 134304
+	</p>
+    <div class="v_space"></div>
+    
+    <div class="title-wrapper">
+        <h2>Mission and Vision</h2>
+    </div>
+    <div class="two_third" style="width:48%;">
+        <h3>Mission</h3>
+            <p>To cater the printing needs of the general public and produce outputs that meet their project specifications with higher standard of workmanship and quality services. </p>
+    </div>
+    <div class="one_third_last" style="width:48%;">
+        <h3>Vision</h3>
+        <p>
+			To be amongst the leading business ventures in printing of personalized items in the region and to become a focal point of other rental and trading businesses. 
+		</p>
+    </div>
+    <iframe height="400" frameborder="0" style="width: 100%;" src="http://maps.google.com/maps?q=13.824586996963804,121.06706139160157&amp;ll=13.824586996963804,121.06706139160157&amp;z=11&amp;output=embed"> </iframe>
+    <div class="clear"></div>
+</li>
+<!--About Us End-->
+<!--Gallery Start-->
+<li id="page_gallery">
+    <div class="title-wrapper">
+        <h2>Gallery</h2>
+    </div>
+    <div>
+    <ul class="splitter" id="filter">
+        <li>
+        <ul>
+            <li class="segment-1 selected-1"><a href="#" data-value="all">All</a></li>
+            <li class="segment-0"><a href="#" data-value="non_veg">Key Chain</a></li>
+            <li class="segment-2"><a href="#" data-value="veg">Mug</a></li>
+            <li class="segment-3"><a href="#" data-value="drinks">Tumbler</a></li>
+			<li class="segment-4"><a href="#" data-value="tshirt">T Shirt</a></li>
+        </ul>
+        </li>
+    </ul>
+    </div>
+    <div class="image_grid">
+        <ul id="list" class="portfolio_list">
+            <li data-id="id-1" class="non_veg">
+                
+                    <span class="roll"></span>
+                    <img src="images/gallery_img_1.jpg" alt="img" class="img_bor" />
+            </li>
+			<li data-id="id-1" class="non_veg">
+                
+                    <span class="roll"></span>
+                    <img src="images/k1.jpg" alt="img" class="img_bor" />
+            </li>
+			<li data-id="id-1" class="non_veg">
+                
+                    <span class="roll"></span>
+                    <img src="images/k2.jpg" alt="img" class="img_bor" />
+            </li>
+		
+		
+            <li data-id="id-2" class="non_veg">
+                    <span class="roll"></span>
+                    <img src="images/gallery_img_2.jpg" alt="img" class="img_bor" />
+            </li>
+            <li data-id="id-3" class="non_veg">
+                    <span class="roll"></span>
+                    <img src="images/gallery_img_3.jpg" alt="img" class="img_bor" />
+</li>
+	
+			<li data-id="id-7" class="drinks">
+                    <span class="roll"></span>
+                    <img src="images/tt5.jpg" alt="img" class="img_bor" />
+            </li>
+		
+            <li data-id="id-9" class="drinks">
+                <span class="roll"></span>
+                <img src="images/gallery_img_9.jpg" alt="img" class="img_bor" />
+            </li>
+			<li data-id="id-1" class="tshirt"><span class="roll"></span>
+
+                    <img src="images/t1.jpg" alt="img" class="img_bor" />
+            </li>
+			<li data-id="id-1" class="tshirt"><span class="roll"></span>
+                    <img src="images/t2.jpg" alt="img" class="img_bor" />
+            </li>
+		
+        </ul>
+    </div>    
+</li>
+<!--Gallery End-->
+
+<!--Contact Start-->
+<li id="page_contact">
+    
+    <div class="clear"></div>
+    <div class="v_space"></div>
+    
+    <div class="title-wrapper">
+        <h2>Feedback Form</h2>
+    </div>
+    <div class="contact_form">
+        <form method="post" action="sendEmail.php" name="contact-form" id="contact-form">	
+		<div id="response"></div>
+        <div id="main">
+            <div class="one_third">
+                <label>Name:</label>
+                <p><input type="text" name="name" id="name" size="30" /></p>
+            </div>
+            <div class="one_third">
+                <label>Email:</label>
+                <p><input type="text" name="email" id="email" size="30" /></p>
+            </div>
+            <div class="one_third_last">
+                <label>Subject:</label>
+                <p><input type="text" name="subject" id="subject" size="30" /></p>
+            </div>
+            <label>Message:</label>
+            <p><textarea name="message" id="message" cols="30" rows="10"></textarea></p>
+            <p><input  class="contact_button button" type="submit" name="submit" id="submit" value="Send" /></p>
+        </div>
+        </form>
+    </div> 
+</li>
+<!--Contact End-->
+
+<!--adminlogin Start-->
+<li id="adminlogin">
+   
+    <div class="title-wrapper">
+        <h2>Admin Login Form</h2>
+    </div>
+    <div class="contact_form">
+        <form method="post" action="login.php" name="contact-form" id="contact-form">	
+        <div id="main">
+            <div class="one_third">
+                <label>Username:</label>
+                <p><input type="text" name="user" id="name" size="30" /></p>
+            </div>
+            <div class="one_third">
+                <label>Password:</label>
+                <p><input type="password" name="password" id="email" size="30" /></p>
+            </div>
+            <div class="one_third_last">
+			<label>&nbsp;</label>
+                <input  class="contact_button button" type="submit" name="submit" id="submit" value="Login" />
+            </div>
+        </div>
+        </form>
+    </div> 
+</li>
+<!--adminlogin End-->
+                   
+</ul>
+</article>
+</div>
+<!--Content End-->
+
+</div>
+<!--Page wrapper End-->
+</body>
+</html>
